06231bde051964ff40d4cde3e985debc521fdb78
war <37655074@qq.com>
2019-03-01 18:46:29 +0800

    初始化代码

--- src/main/java/com/imooc/TestController.java
+++ src/main/java/com/imooc/TestController.java
@@ -3,6 +3,7 @@ package com.imooc;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /**
  * Created by war on 2019/3/1.
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class TestController {
 
     @RequestMapping("/test")
+    @ResponseBody
     public String test(){
         return "test";
     }
